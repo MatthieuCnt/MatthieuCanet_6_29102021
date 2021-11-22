4140d0fc489e80c353016d090cbd346efc7e5ef8
Matthieu Canet <mattcanet@gmail.com>
2021-11-22 17:03:46 +0100

    photographer js

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,26 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+	const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+	const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+	function getUserCardDOM() {
+		const article = document.createElement('article');
+		const img = document.createElement('img');
+		img.setAttribute('src', picture);
+		const h2 = document.createElement('h2');
+		h2.textContent = name;
+		const h1 = document.createElement('h1');
+		h1.textContent = city.concat(', ', country);
+		const p = document.createElement('p');
+		p.textContent = tagline;
+		const div = document.createElement('div');
+		div.textContent = price + '€/jour';
+		article.appendChild(img);
+		article.appendChild(h2);
+		article.appendChild(h1);
+		article.appendChild(p);
+		article.appendChild(div);
+		return article;
+	}
+	return { name, picture, getUserCardDOM };
 }
