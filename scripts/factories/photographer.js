c366345df84218cf204eaaafc0fbc542baa0e40e
Matthieu Canet <mattcanet@gmail.com>
2021-12-09 19:01:02 +0100

    changes

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -24,3 +24,27 @@ function photographerFactory(data) {
 	}
 	return { name, picture, getUserCardDOM };
 }
+
+function photographerpage(data) {
+	const { name, portrait, city, country, tagline } = data;
+
+	const picture = `assets/photographers/${portrait}`;
+
+	function getUserCardDOM2() {
+		const article = document.createElement('article');
+		const img = document.createElement('img');
+		img.setAttribute('src', picture);
+		const h2 = document.createElement('h2');
+		h2.textContent = name;
+		const h1 = document.createElement('h1');
+		h1.textContent = city.concat(', ', country);
+		const p = document.createElement('p');
+		p.textContent = tagline;
+		article.appendChild(img);
+		article.appendChild(h2);
+		article.appendChild(h1);
+		article.appendChild(p);
+		return article;
+	}
+	return { name, picture, getUserCardDOM2 };
+}
